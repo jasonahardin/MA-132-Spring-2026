df8629ed993d64414cfe02962ecf5b8a6fc76f45
Jason Hardin <jhardin@worcester.edu>
2026-01-28 17:07:34 +0000

    Latest build deployed.

---
+++ course/lunr-pretext-search-index.js
@@ -0,0 +1,140 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "syllabus",
+  "level": "1",
+  "url": "syllabus.html",
+  "type": "Section",
+  "number": "",
+  "title": "Syllabus",
+  "body": " Syllabus        Course Information  This is the syllabus for course name (MATH xxx, section xxx) for [term] 20xx. It is a [n] credit course.    Instructor  Prof. Lastname, Office Location, prof.lastname@example.edu .    Student Hours  TBD    Class meets  course times and location.    Course Description  course description from catalog    Prerequisite  list of prerequisites    Textbook and course materials   textbook name by textbook author.       Course Overview        Assessments and Grades     "
+},
+{
+  "id": "sec-course-info-2",
+  "level": "2",
+  "url": "syllabus.html#sec-course-info-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "course name (MATH xxx, section xxx) "
+},
+{
+  "id": "notes-week-01",
+  "level": "1",
+  "url": "notes-week-01.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 1",
+  "body": " Week 1   This is an outline of the topics we covered in the first week of class.     Monday 8\/22      Wednesday 8\/24      Friday 8\/26     "
+},
+{
+  "id": "notes-week-02",
+  "level": "1",
+  "url": "notes-week-02.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 2",
+  "body": " Week 2   Monday      Wednesday      Friday     "
+},
+{
+  "id": "activity-01-intro-activity",
+  "level": "1",
+  "url": "activity-01-intro-activity.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Introduction Activity",
+  "body": " Introduction Activity    This is the introduction to the activity.      This is the first exercise.    "
+},
+{
+  "id": "activity-01-intro-activity-3",
+  "level": "2",
+  "url": "activity-01-intro-activity.html#activity-01-intro-activity-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  This is the first exercise.   "
+},
+{
+  "id": "practice-LT01-MeasurementWordProblems",
+  "level": "1",
+  "url": "practice-LT01-MeasurementWordProblems.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "LT 1 Practice Problems",
+  "body": " LT 1 Practice Problems    Provide complete \"Teacher's Solutions\" with bar diagrams and work in units for the following measurement word problems.      Anna poured liters of water into an empty fish tank. If of the fish tank was filled, find the capacity of the tank.      Segment is times as long as segment . Segment is cm longer than segment . If is cm long, how long is ?      Henry bought liter of fruit juice. He kept liter of it in a bottle and poured the remainder equally into cups. How much fruit juice was there in each cup? Give your final answer in liters; it should be a fraction.      The length of three rods are in the ratio . If the total length is cm, find the length of the largest rod.      After cutting off a length of m from a rope m long, the remainder is divided into equal pieces. What is the length of each piece? Give your answer in meters; final answer should be a decimal.      A tank was filled with water. After another liters of water were poured in, the tank becomes full. How much water does the tank hold?    "
+},
+{
+  "id": "practice-LT01-MeasurementWordProblems-3",
+  "level": "2",
+  "url": "practice-LT01-MeasurementWordProblems.html#practice-LT01-MeasurementWordProblems-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Anna poured liters of water into an empty fish tank. If of the fish tank was filled, find the capacity of the tank.   "
+},
+{
+  "id": "practice-LT01-MeasurementWordProblems-4",
+  "level": "2",
+  "url": "practice-LT01-MeasurementWordProblems.html#practice-LT01-MeasurementWordProblems-4",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Segment is times as long as segment . Segment is cm longer than segment . If is cm long, how long is ?   "
+},
+{
+  "id": "practice-LT01-MeasurementWordProblems-5",
+  "level": "2",
+  "url": "practice-LT01-MeasurementWordProblems.html#practice-LT01-MeasurementWordProblems-5",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Henry bought liter of fruit juice. He kept liter of it in a bottle and poured the remainder equally into cups. How much fruit juice was there in each cup? Give your final answer in liters; it should be a fraction.   "
+},
+{
+  "id": "practice-LT01-MeasurementWordProblems-6",
+  "level": "2",
+  "url": "practice-LT01-MeasurementWordProblems.html#practice-LT01-MeasurementWordProblems-6",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  The length of three rods are in the ratio . If the total length is cm, find the length of the largest rod.   "
+},
+{
+  "id": "practice-LT01-MeasurementWordProblems-7",
+  "level": "2",
+  "url": "practice-LT01-MeasurementWordProblems.html#practice-LT01-MeasurementWordProblems-7",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "  After cutting off a length of m from a rope m long, the remainder is divided into equal pieces. What is the length of each piece? Give your answer in meters; final answer should be a decimal.   "
+},
+{
+  "id": "practice-LT01-MeasurementWordProblems-8",
+  "level": "2",
+  "url": "practice-LT01-MeasurementWordProblems.html#practice-LT01-MeasurementWordProblems-8",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": "  A tank was filled with water. After another liters of water were poured in, the tank becomes full. How much water does the tank hold?   "
+},
+{
+  "id": "homework",
+  "level": "1",
+  "url": "homework.html",
+  "type": "Chapter",
+  "number": "",
+  "title": "Homework",
+  "body": " Homework    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
